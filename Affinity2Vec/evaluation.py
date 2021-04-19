2a9690b93fef91d7de6be770a1f3876d14a8f41e
MAHA <mthafar@kent.edu>
2021-04-19 15:23:32 +0300

    Add files via upload

---
+++ Affinity2Vec/evaluation.py
@@ -0,0 +1,108 @@
+# Evaluation metrics that used to assest the regressors and other metrics in main code
+
+import numpy as np
+import os
+import subprocess
+
+
+def mse(y,f):
+    
+    mse = ((y - f)**2).mean(axis=0)
+    return mse
+#----------------------------------------------------
+
+def get_cindex(Y, P):
+    
+    summ = 0
+    pair = 0
+    
+    for i in range(1, len(Y)):
+        for j in range(0, i):
+            if i != j:
+                if(Y[i] > Y[j]):
+                    pair +=1
+                    summ +=  1* (P[i] > P[j]) + 0.5 * (P[i] == P[j])
+            
+    if pair != 0:
+        return summ/pair
+    
+    else:
+        return 0
+
+#------------------------------------------------------------------------
+def get_aupr(Y, P,threshold):
+    
+    if hasattr(Y, 'A'): Y = Y.A
+    if hasattr(P, 'A'): P = P.A
+    Y = np.where(Y>threshold, 1, 0)
+    Y = Y.ravel()
+    P = P.ravel()
+    
+    f = open("aupr/P_Y.txt", 'w')
+    for i in range(Y.shape[0]):
+        f.write("%f %d\n" %(P[i], Y[i]))
+    f.close()
+    
+    f = open("aupr/aupr_metric.txt", 'w')
+    subprocess.call(["java", "-jar", "aupr/auc.jar", "aupr/P_Y.txt", "list"], stdout=f)
+    f.close()
+    
+    f = open("aupr/aupr_metric.txt")
+    lines = f.readlines()
+    aucpr = float(lines[-2].split()[-1])
+    f.close()
+    
+    return aucpr
+
+#----------------------------------------------------
+
+def r_squared_error(y_actual,y_pred):
+    
+    y_actual = np.array(y_actual)
+    y_pred = np.array(y_pred)
+    y_actual_mean = [np.mean(y_actual) for y in y_actual]
+    y_pred_mean = [np.mean(y_pred) for y in y_pred]
+
+    mult = sum((y_pred - y_pred_mean) * (y_actual - y_actual_mean))
+    mult = mult * mult
+
+    y_obs_sq = sum((y_actual - y_actual_mean)*(y_actual - y_actual_mean))
+    y_pred_sq = sum((y_pred - y_pred_mean) * (y_pred - y_pred_mean) )
+
+    return mult / float(y_obs_sq * y_pred_sq)
+#----------------------------------------------------
+
+def get_k(y_actual,y_pred):
+    
+    y_actual = np.array(y_actual)
+    y_pred = np.array(y_pred)
+
+    return sum(y_actual*y_pred) / float(sum(y_pred*y_pred))
+#----------------------------------------------------
+
+def squared_error_zero(y_actual,y_pred):
+    
+    k = get_k(y_actual,y_pred)
+
+    y_actual = np.array(y_actual)
+    y_pred = np.array(y_pred)
+    y_actual_mean = [np.mean(y_actual) for y in y_actual]
+    upp = sum((y_actual - (k*y_pred)) * (y_actual - (k* y_pred)))
+    down= sum((y_actual - y_actual_mean)*(y_actual - y_actual_mean))
+
+    return 1 - (upp / float(down))
+#----------------------------------------------------
+def get_rm2(ys_orig,ys_line):
+    
+    r2 = r_squared_error(ys_orig, ys_line)
+    r02 = squared_error_zero(ys_orig, ys_line)
+
+    return r2 * (1 - np.sqrt(np.absolute((r2*r2)-(r02*r02))))
+#----------------------------------------------------   
+
+def pearson(y,f):
+    
+    rp = np.corrcoef(y, f)[0,1]
+    return rp
+
+##################################################################
---
+++ Affinity2Vec/training_functions.py
@@ -0,0 +1,131 @@
+'''
+*******************************************************
+Some functions that are needed for training process..
+*******************************************************
+'''
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+import scipy
+from scipy import stats
+import scipy.spatial
+from sklearn.preprocessing import MinMaxScaler
+import collections
+
+#---------------------------------------------------------------------------------------------
+#### ALL needed functions for training process
+#---------------------------------------------------------------------------------------------
+
+######### TRANING FUNCTION ###############
+def Mask_test_index(test_idx, x, DrTr, drugID, targetID):
+    
+    DrTr_train = deepcopy(DrTr)
+    # get the drug index and target index 
+    # mask drug,target = 1 of test data to be 0 (i.e. remove the edge)
+    for i in test_idx:
+
+        dr = x[i,0]
+        dr_index = drugID[dr]
+
+        tr = x[i,1]
+        tr_index = targetID[tr]
+
+        DrTr_train[dr_index, tr_index] = 0
+    
+    return DrTr_train
+
+##----------------------------------------------------
+
+## normalize simiarities to be in positive range [0,1]
+def normalizedMatrix(matrix):
+    
+#     scaler = MinMaxScaler(copy=True, feature_range=(0, 1))
+#     scaler.fit(matrix)
+#     normMat = scaler.transform(matrix)
+    normMat = (matrix - matrix.min()) / (matrix.max() - matrix.min())
+
+    return normMat
+
+#--------------------------------------------------------
+def Strongest_k_sim(Mat,K):
+    
+    m,n = Mat.shape
+
+    Ssim = np.zeros((m,n))
+    for i in range(m):
+        index =  np.argsort(Mat[i,:])[-K:] # sort based on strongest k edges
+        Ssim[i,index] = Mat[i,index] # keep only the nearest neighbors (strongest k edges)
+    
+    np.fill_diagonal(Ssim , 1)  
+     
+    return Ssim
+
+#---------------------------------------------------------------------------------------------
+
+## To compute drug-drug FV cosine similarity and target-target FV cosine similarity
+
+def Cosine_Similarity(Matrix):
+    cos_sim_m = np.zeros((Matrix.shape[0],Matrix.shape[0]))
+    for item_i in range(Matrix.shape[0]):
+        for item_j in range(Matrix.shape[0]):
+            cos_sim_m[item_i][item_j] = 1-(scipy.spatial.distance.cosine(Matrix[item_i,:],Matrix[item_j,:]))
+    
+    return cos_sim_m
+
+#---------------------------------------------------------------------
+## Get Similarity of targets or drugs within specific threshold
+def keep_sim_threshold(simMat, threshold):
+
+    newMat = np.zeros(simMat.shape)
+    #print(newMat)
+    for i,x in enumerate(simMat):
+    #print(i)
+        for j,y in enumerate(x):
+            #print(i,j)
+#             if y <= threshold:
+#                  simMat[i,j] = 0.0
+            if y >= threshold:
+                newMat[i,j] = simMat[i,j]
+            
+            # print(newMat)
+            
+            if (np.count_nonzero(newMat[i]) == 0):
+                col = np.argmax(simMat[i])
+                # print('col', col)
+                newMat[i,col] = simMat[i,col] 
+
+    return newMat
+#----------------------------------------------
+
+#### Encoding Functions ###################
+###########################################
+def integer_encoding(s,dic,maxLen):
+    
+    s_labelE = np.zeros(maxLen)
+    
+    for i,char in enumerate(s[:maxLen]):
+        s_labelE[i] = dic[char]
+        
+
+    return s_labelE
+############################################3
+def oneHOT_encoding(s, dic, maxLen):
+    
+    oneHot_e = np.zeros((maxLen, len(dic)))
+    
+    for i,char in enumerate(s[:maxLen]):
+        oneHot_e[i, (dic[char])-1] = 1
+    
+    return oneHot_e
+
+#--------------------------------
+def get_unique_tokens(allSMILES):
+    all_smiles_char = []
+    for i in range(len(allSMILES)):
+        all_smiles_char += str(allSMILES[i])
+
+    tokens = sorted(list(set(all_smiles_char)))
+    num_Tokens = len(tokens)
+    
+    return tokens, num_Tokens
+#--------------------------------------------------------------------
